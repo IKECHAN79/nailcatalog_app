edf98100ea7964f3ae28fba7ffd1e2956bff9c89
IKECHAN79 <saki07098@gmail.com>
2022-07-01 15:39:50 +0900

    ユーザー管理機能④

--- app/controllers/nails_controller.rb
+++ app/controllers/nails_controller.rb
@@ -1,4 +1,5 @@
 class NailsController < ApplicationController
   def index
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-
   devise_for :users
 
-  get 'nails/index'
   root to: "nails#index"
 end
